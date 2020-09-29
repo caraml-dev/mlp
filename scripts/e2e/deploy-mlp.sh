eb41d94c16b721997319c042eabf16d1dcfeee39
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2020-09-30 04:54:22 +0700

    Fix fetching GitHub ref

--- scripts/e2e/deploy-mlp.sh
+++ scripts/e2e/deploy-mlp.sh
@@ -9,13 +9,13 @@ CHART_PATH="$1"
 helm install mlp ${CHART_PATH} --namespace=mlp \
     --values=${CHART_PATH}/values/values.yaml \
     --values=${CHART_PATH}/values/e2e-values.yaml \
-    --set mlp.image.tag=${GITHUB_REF} \
+    --set mlp.image.tag=${GITHUB_REF#refs/heads/} \
     --dry-run
 
 helm install mlp ${CHART_PATH} --namespace=mlp \
     --values=${CHART_PATH}/values/values.yaml \
     --values=${CHART_PATH}/values/e2e-values.yaml \
-    --set mlp.image.tag=${GITHUB_REF} \
+    --set mlp.image.tag=${GITHUB_REF#refs/heads/} \
     --wait
 
 kubectl get all --namespace=mlp
