bec063a1e9f7b5991eea5293b45f65fddf4f00ec
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2020-09-30 10:25:29 +0700

    Move chart's values to chart directory from chart/values. This is the right place for them

--- scripts/e2e/deploy-mlp.sh
+++ scripts/e2e/deploy-mlp.sh
@@ -7,15 +7,13 @@ set -o pipefail
 CHART_PATH="$1"
 
 helm install mlp ${CHART_PATH} --namespace=mlp \
-    --values=${CHART_PATH}/values/values.yaml \
-    --values=${CHART_PATH}/values/e2e-values.yaml \
+    --values=${CHART_PATH}/values-e2e.yaml \
     --set mlp.image.tag=${GITHUB_REF#refs/heads/} \
     --dry-run
 
 helm install mlp ${CHART_PATH} --namespace=mlp \
-    --values=${CHART_PATH}/values/values.yaml \
-    --values=${CHART_PATH}/values/e2e-values.yaml \
+    --values=${CHART_PATH}/values-e2e.yaml \
     --set mlp.image.tag=${GITHUB_REF#refs/heads/} \
-    --wait
+    --wait --timeout=5m
 
 kubectl get all --namespace=mlp
