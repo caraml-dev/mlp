62def192616647e17c4954f0e3d167b3b197c3c3
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2020-10-07 15:27:48 +0700

    Fix parsing image tag in e2e (#7)

--- scripts/e2e/deploy-mlp.sh
+++ scripts/e2e/deploy-mlp.sh
@@ -8,12 +8,12 @@ CHART_PATH="$1"
 
 helm install mlp ${CHART_PATH} --namespace=mlp \
     --values=${CHART_PATH}/values-e2e.yaml \
-    --set mlp.image.tag=${GITHUB_REF#refs/heads/} \
+    --set mlp.image.tag=${GITHUB_REF#refs/*/} \
     --dry-run
 
 helm install mlp ${CHART_PATH} --namespace=mlp \
     --values=${CHART_PATH}/values-e2e.yaml \
-    --set mlp.image.tag=${GITHUB_REF#refs/heads/} \
+    --set mlp.image.tag=${GITHUB_REF#refs/*/} \
     --wait --timeout=5m
 
 kubectl get all --namespace=mlp
