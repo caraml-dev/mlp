106a7d857d1e2c250e6be4bb00132bb954b48d13
Terence Lim <terencelimxp@gmail.com>
2021-03-15 14:29:33 +0800

    Add Feast to MLP
    Signed-off-by: Terence Lim <terencelimxp@gmail.com>

---
+++ db-migrations/6_add_feast_app.up.sql
@@ -0,0 +1,12 @@
+INSERT INTO applications
+(
+    name,
+    href,
+    description,
+    icon,
+    use_projects,
+    is_in_beta,
+    is_disabled
+)
+VALUES
+    ('Feast', '/feast', 'Platform for managing and serving ML features', 'appSearchApp', TRUE, TRUE, FALSE);
