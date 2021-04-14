d4b6778081a314921b7fddabd801f14aa4dd2503
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2021-04-14 14:54:47 +0700

    Fix lint

--- ui/packages/app/src/services/merlin/Merlin.js
+++ ui/packages/app/src/services/merlin/Merlin.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ApplicationsContext } from "@gojek/mlp-ui";
 
 export const MerlinApp = () => {
