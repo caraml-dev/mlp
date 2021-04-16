fbfb4cd651662a4473c838d53bc69af7e65ca1b7
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2021-04-16 12:01:30 +0700

    Fix lint step

--- ui/packages/app/src/pages/project/components/MerlinDeployments.js
+++ ui/packages/app/src/pages/project/components/MerlinDeployments.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { navigate } from "@reach/router";
 import { EuiLink, EuiListGroup, EuiText } from "@elastic/eui";
 
 export const MerlinDeployments = ({ project, models }) => {
