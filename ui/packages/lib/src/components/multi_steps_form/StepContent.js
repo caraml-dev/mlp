be0d3b4d5eb2239bec39c870c4976cb45705aea2
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2021-05-09 06:22:20 +0700

    Make step content's width on steps wizard form configurable

--- ui/packages/lib/src/components/multi_steps_form/StepContent.js
+++ ui/packages/lib/src/components/multi_steps_form/StepContent.js
@@ -1,5 +1,5 @@
-import { EuiFlexGroup, EuiFlexItem } from "@elastic/eui";
 import React from "react";
+import { EuiFlexGroup, EuiFlexItem } from "@elastic/eui";
 
 export const StepContent = ({ children, width = "75%" }) => (
   <EuiFlexGroup direction="row" justifyContent="center">
