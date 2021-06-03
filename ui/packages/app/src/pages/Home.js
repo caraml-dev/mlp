1745a947e6040baadab9dd8949394199c4078cb0
Tio Pramayudi <itok54@gmail.com>
2021-06-03 13:33:27 +0700

    Redirect to last selected project id (#22)
    * Redirect to last selected project id
    
    * Remove unnecessary import
    
    * Fix lint warning
    
    * Remove unnecessary variable
    
    Co-authored-by: Tio Pramayudi <tio.pramayudi@gojek.com>

--- ui/packages/app/src/pages/Home.js
+++ ui/packages/app/src/pages/Home.js
@@ -1,50 +1,19 @@
-import React, { Fragment } from "react";
-import {
-  EuiButton,
-  EuiCard,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiIcon,
-  EuiFlexGrid,
-  EuiEmptyPrompt
-} from "@elastic/eui";
-import { ApplicationsContext } from "@gojek/mlp-ui";
+import React, { useContext } from "react";
+import { EuiLoadingContent, EuiPage, EuiPageBody } from "@elastic/eui";
+import { CurrentProjectContext } from "@gojek/mlp-ui";
+import { Redirect } from "@reach/router";
 
 const Home = () => {
-  return (
-    <Fragment>
-      <EuiEmptyPrompt
-        iconType="machineLearningApp"
-        title={<h2>Machine Learning Platform</h2>}
-      />
-      <EuiFlexGroup style={{ paddingLeft: 50, paddingRight: 50 }}>
-        <EuiFlexItem grow={false} style={{ margin: "auto" }}>
-          <EuiFlexGrid columns={3}>
-            <ApplicationsContext.Consumer>
-              {({ apps }) =>
-                apps.map((app, idx) => {
-                  return (
-                    <EuiFlexItem key={idx}>
-                      <EuiCard
-                        icon={<EuiIcon size="xxl" type={app.icon} />}
-                        title={app.name}
-                        description={app.description}
-                        footer={
-                          <EuiButton href={app.href}>
-                            Go to {app.name}
-                          </EuiButton>
-                        }
-                        betaBadgeLabel={app.is_in_beta ? "Beta" : undefined}
-                      />
-                    </EuiFlexItem>
-                  );
-                })
-              }
-            </ApplicationsContext.Consumer>
-          </EuiFlexGrid>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    </Fragment>
+  const { projectId } = useContext(CurrentProjectContext);
+
+  return projectId === null ? (
+    <EuiPage>
+      <EuiPageBody>
+        <EuiLoadingContent lines={5} />
+      </EuiPageBody>
+    </EuiPage>
+  ) : (
+    <Redirect to={`/projects/${projectId}`} noThrow />
   );
 };
 
