c093e94e27e4039a55370f571db1be10a82f6c95
Krithika Sundararajan <krithika.sundararajan@go-jek.com>
2021-10-21 09:42:31 +0800

    Use json-bingint to parse JSON body

--- ui/packages/lib/src/utils/parseJson.js
+++ ui/packages/lib/src/utils/parseJson.js
@@ -1,5 +1,11 @@
+const jsonBig = require(`json-bigint`);
+
 export const parseJson = response => {
-  return response.json().catch(error => {
+  // using the json-bigint library to parse the data instead of the Response.json() Web API,
+  // to parse BigInt without losing precision.
+  return response.text().then(
+    text => jsonBig.parse(text)
+  ).catch(error => {
     // for responses without body
     if (error.name === "SyntaxError") {
       return {};
