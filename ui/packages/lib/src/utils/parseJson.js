64ad63cda4fdf3541507c1ecd532ed767b6f5ac4
Krithika Sundararajan <krithika369@live.com>
2021-10-21 14:53:17 +0800

    Use json-bingint to parse JSON body (#38)
    * Use json-bingint to parse JSON body
    
    * Add parseBigInt as an option in fetchJson
    
    Co-authored-by: Krithika Sundararajan <krithika.sundararajan@go-jek.com>

--- ui/packages/lib/src/utils/parseJson.js
+++ ui/packages/lib/src/utils/parseJson.js
@@ -1,5 +1,15 @@
-export const parseJson = response => {
-  return response.json().catch(error => {
+const jsonBig = require(`json-bigint`);
+
+const parseJsonHandleBigInt = (response, parseBigInt) => {
+  // If parseBigInt is set, use the json-bigint library to parse the data instead of
+  // the Response.json() Web API which would lose precision.
+  return parseBigInt
+    ? response.text().then(text => jsonBig.parse(text))
+    : response.json();
+}
+
+export const parseJson = (response, parseBigInt) => {
+  return parseJsonHandleBigInt(response, parseBigInt).catch(error => {
     // for responses without body
     if (error.name === "SyntaxError") {
       return {};
