c28dca2aeef91a9d476498dd055fb53f9d7b1392
Ewe Zi Yi <36802364+deadlycoconuts@users.noreply.github.com>
2023-03-29 11:07:35 +0800

    Add additional helper method to init Google Client from specified file path (#79)
    * Add additional helper method to init Google Client from specified filepath
    
    * Fix lint comments by shortening line

--- api/pkg/auth/google_auth.go
+++ api/pkg/auth/google_auth.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -48,7 +49,7 @@ func (s *idTokenSource) Token() (*oauth2.Token, error) {
 }
 
 // InitGoogleClient is a helper method to be used by CaraML components to initialise a Google Client that appends ID
-// tokens to the headers of all outgoing requests with ID tokens, regardless of the type of credentials used
+// tokens to the headers of all outgoing requests, regardless of the type of credentials used
 func InitGoogleClient(ctx context.Context) (*http.Client, error) {
 	cred, err := google.FindDefaultCredentials(ctx)
 	if err != nil {
@@ -64,11 +65,45 @@ func InitGoogleClient(ctx context.Context) (*http.Client, error) {
 		return idtoken.NewClient(ctx, defaultCaraMLAudience)
 	}
 
+	return getGoogleClientFromNonServiceAccountCredentials(ctx, cred)
+}
+
+// InitGoogleClientFromCredentialsFile is a helper method to be used by CaraML components to initialise a Google Client
+// that appends ID tokens to the headers of all outgoing requests, regardless of the type of credentials that are
+// located at the location of the given filepath
+func InitGoogleClientFromCredentialsFile(ctx context.Context, filepath string) (*http.Client, error) {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+	cred, err := google.CredentialsFromJSON(context.Background(), data)
+	if err != nil {
+		return nil, err
+	}
+
+	var f credentialsFile
+	if err := json.Unmarshal(cred.JSON, &f); err != nil {
+		return nil, err
+	}
+
+	if f.Type == serviceAccountKey {
+		return idtoken.NewClient(ctx, defaultCaraMLAudience, idtoken.WithCredentialsFile(filepath))
+	}
+
+	return getGoogleClientFromNonServiceAccountCredentials(ctx, cred)
+}
+
+// getGoogleClientFromNonServiceAccountCredentials is a helper method to wrap the given non-service account credentials
+// in a Google HTTP client that appends ID tokens to the headers of all outgoing requests
+func getGoogleClientFromNonServiceAccountCredentials(
+	context context.Context,
+	cred *google.Credentials,
+) (*http.Client, error) {
 	tokenSource := oauth2.ReuseTokenSource(nil, &idTokenSource{TokenSource: cred.TokenSource})
 
 	var opts []idtoken.ClientOption
 	opts = append(opts, option.WithTokenSource(tokenSource))
-	t, err := htransport.NewTransport(ctx, http.DefaultTransport, opts...)
+	t, err := htransport.NewTransport(context, http.DefaultTransport, opts...)
 	if err != nil {
 		return nil, err
 	}
