595fd7d1ac9b3af80f5f20c7556ab36cb7fc4b97
Alexander <alexander.i.interns@aux.gojek.com>
2023-04-10 13:52:46 +0700

    refactor: change definition order, struct naming

--- api/pkg/artifact/artifact.go
+++ api/pkg/artifact/artifact.go
@@ -8,37 +8,22 @@ import (
 	"google.golang.org/api/iterator"
 )
 
-type GcsArtifactClient struct {
-	API *storage.Client
-}
-
-type NopArtifactClient struct{}
-
-func NewGcsArtifactClient(api *storage.Client) Service {
-	return &GcsArtifactClient{
-		API: api,
-	}
-}
-
-func NewNopArtifactClient() Service {
-	return &NopArtifactClient{}
-}
-
 type Service interface {
 	DeleteArtifact(ctx context.Context, url string) error
 }
 
-func (ad *NopArtifactClient) DeleteArtifact(ctx context.Context, url string) error {
-	return nil
+type GcsArtifactClient struct {
+	API *storage.Client
 }
-func (gc *GcsArtifactClient) DeleteArtifact(ctx context.Context, url string) error {
+
+func (gac *GcsArtifactClient) DeleteArtifact(ctx context.Context, url string) error {
 	// Get bucket name and gcsPrefix
 	// the [5:] is to remove the "gs://" on the artifact uri
 	// ex : gs://bucketName/path → bucketName/path
-	gcsBucket, gcsLocation := gc.getGcsBucketAndLocation(url[5:])
+	gcsBucket, gcsLocation := gac.getGcsBucketAndLocation(url[5:])
 
 	// Sets the name for the bucket.
-	bucket := gc.API.Bucket(gcsBucket)
+	bucket := gac.API.Bucket(gcsBucket)
 
 	it := bucket.Objects(ctx, &storage.Query{
 		Prefix: gcsLocation,
@@ -58,9 +43,25 @@ func (gc *GcsArtifactClient) DeleteArtifact(ctx context.Context, url string) err
 	return nil
 }
 
-func (gc *GcsArtifactClient) getGcsBucketAndLocation(str string) (string, string) {
+func (gac *GcsArtifactClient) getGcsBucketAndLocation(str string) (string, string) {
 	// Split string using delimiter
 	// ex : bucketName/path/path1/item → (bucketName , path/path1/item)
 	splitStr := strings.SplitN(str, "/", 2)
 	return splitStr[0], splitStr[1]
+}
+
+func NewGcsArtifactClient(api *storage.Client) Service {
+	return &GcsArtifactClient{
+		API: api,
+	}
+}
+
+type NopArtifactClient struct{}
+
+func (nac *NopArtifactClient) DeleteArtifact(ctx context.Context, url string) error {
+	return nil
+}
+
+func NewNopArtifactClient() Service {
+	return &NopArtifactClient{}
 }
