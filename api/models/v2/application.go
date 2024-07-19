69de678a2bd8b6d728f180ffd4f15c5e08939d90
ewezy <ziyi.ewe@gojek.com>
2024-07-19 16:08:19 +0800

    Update mlp app to serve new configs

--- api/models/v2/application.go
+++ api/models/v2/application.go
@@ -1,10 +1,11 @@
 package models
 
 type Application struct {
-	Name          string             `json:"name" validate:"required"`
-	Description   string             `json:"description"`
-	Homepage      string             `json:"homepage"`
-	Configuration *ApplicationConfig `json:"config" validate:"dive"`
+	Name              string             `json:"name" validate:"required"`
+	Description       string             `json:"description"`
+	Homepage          string             `json:"homepage"`
+	Configuration     *ApplicationConfig `json:"config" validate:"dive"`
+	IsProjectAgnostic bool               `json:"is_project_agnostic"`
 }
 
 type ApplicationConfig struct {
